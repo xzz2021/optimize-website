9a8df846dc9b4a7ce6b3f303264327281b54ad15
xzz2021 <zipeng.2016@gmail.com>
2025-12-08 10:48:23 +0800

    调整b站功能

--- src/pages/bilibili/fn.ts
+++ src/pages/bilibili/fn.ts
@@ -1,3 +1,7 @@
+import { chromeStorage } from "@/utils/chromeStorage"
+import $ from "jquery"
+
+
 //  自动点击 试用高清
 export const autoHD = async () => {
   // 第一次进入页面 直接执行一次
@@ -6,18 +10,65 @@ export const autoHD = async () => {
   const oldHref = window.location.href
   //  监听url变化
   checkUrl(oldHref)
-  const fullscreen = () => {
-    const playerWrapper = document.querySelector(".bpx-player-video-wrap")
-    const video = playerWrapper?.querySelector("video")
-    console.log("🚀 ~ fullscreen ~ video:", video)
-    video?.requestFullscreen()
-  }
 
-  setInterval(() => {
-    fullscreen()
-  }, 3000)
+  closeDanmu()
+  // 瀑布流加载滚动时执行
+  removeItem()
+
+  checkStop()
+
 }
 
+const closeDanmu = () => {
+  const aaa = setInterval(()=> {
+    const isOpen = document.querySelector('.bpx-player-dm-setting.disabled')
+  if(isOpen && isOpen instanceof HTMLElement){
+    isOpen.click()
+    clearInterval(aaa)
+  }
+  }, 2000)
+}
+
+
+const removeItem = async () => {
+  const url = window.location.href
+  if(url.includes("/video/"))return 
+    setTimeout(() => {
+      removeItem()
+}, 2000)
+  const localListLength = await chromeStorage.get("listLength") as number
+  const list = $(".bili-video-card")
+  if(list.length == localListLength) return
+  const localList = await chromeStorage.get("localList") as string[] || []
+	console.log("TCL: removeItem -> localList", localList)
+  $('.floor-single-card').each(function(){
+    $(this).remove()
+  })
+  list.each(   function () {
+    const authorDom = $(this).find(".bili-video-card__info--author")
+    if(authorDom.attr("hadflag") == "true") return
+    // 使用$ 创建一个dom
+    const newDom = $('<div style="z-index: 99999; cursor: pointer; background: #ea4f87; color: white; text-align: center; margin-left: 15px">屏蔽</div>')
+    authorDom.parent().parent().append(newDom)
+    authorDom.attr("hadflag", "true")
+    const authorName = authorDom?.text()
+    // 给newDom添加点击事件 点击后 删除当前dom
+    newDom.on("click", async () => {
+      chromeStorage.set({localList: [...localList, authorName]}) 
+    const ll = await chromeStorage.get("listLength") as number
+        
+      chromeStorage.set({listLength: ll - 1})
+      $(this).hide()
+    })
+    if(localList.includes(authorName)){
+      $(this).hide()
+    // const ll = await chromeStorage.get("listLength") as number
+    //   chromeStorage.set({listLength: ll - 1})
+    }
+
+  })
+
+}
 const clickHD = () => {
   if (validUrl()) {
     const timer4Btn = setInterval(async () => {
@@ -25,50 +76,52 @@ const clickHD = () => {
       if (trialBtn && trialBtn instanceof HTMLElement) {
         clearInterval(timer4Btn)
         trialBtn.click()
+      }else{
+        window.localStorage.clear()
       }
     }, 1000)
   }
 }
 
-export const fullscreen = () => {
-  const playerWrapper = document.querySelector(".bpx-player-video-wrap")
+// export const fullscreen = () => {
+//   const playerWrapper = document.querySelector(".bpx-player-video-wrap")
 
-  const video = playerWrapper?.querySelector("video")
-  console.log("🚀 ~ fullscreen ~ video:", video)
-  video?.requestFullscreen()
-  return
-  if (!video) return
-  console.log("✨ 🍰 ✨ xzz2021: fullscreen -> video", video)
-  // 全屏切换函数
-  if (
-    document.fullscreenElement ||
-    document.webkitFullscreenElement ||
-    document.mozFullScreenElement ||
-    document.msFullscreenElement
-  ) {
-    // 如果当前已经是全屏模式，则退出全屏
-    if (document.exitFullscreen) {
-      document.exitFullscreen()
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen()
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen()
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen()
-    }
-  } else {
-    // 如果当前不是全屏模式，则进入全屏
-    if (video.requestFullscreen) {
-      video.requestFullscreen()
-    } else if (video.webkitRequestFullscreen) {
-      video.webkitRequestFullscreen()
-    } else if (video.mozRequestFullScreen) {
-      video.mozRequestFullScreen()
-    } else if (video.msRequestFullscreen) {
-      video.msRequestFullscreen()
-    }
-  }
-}
+//   const video = playerWrapper?.querySelector("video")
+//   console.log("🚀 ~ fullscreen ~ video:", video)
+//   video?.requestFullscreen()
+//   return
+//   if (!video) return
+//   console.log("✨ 🍰 ✨ xzz2021: fullscreen -> video", video)
+//   // 全屏切换函数
+//   if (
+//     document.fullscreenElement ||
+//     document.webkitFullscreenElement ||
+//     document.mozFullScreenElement ||
+//     document.msFullscreenElement
+//   ) {
+//     // 如果当前已经是全屏模式，则退出全屏
+//     if (document.exitFullscreen) {
+//       document.exitFullscreen()
+//     } else if (document.webkitExitFullscreen) {
+//       document.webkitExitFullscreen()
+//     } else if (document.mozCancelFullScreen) {
+//       document.mozCancelFullScreen()
+//     } else if (document.msExitFullscreen) {
+//       document.msExitFullscreen()
+//     }
+//   } else {
+//     // 如果当前不是全屏模式，则进入全屏
+//     if (video.requestFullscreen) {
+//       video.requestFullscreen()
+//     } else if (video.webkitRequestFullscreen) {
+//       video.webkitRequestFullscreen()
+//     } else if (video.mozRequestFullScreen) {
+//       video.mozRequestFullScreen()
+//     } else if (video.msRequestFullscreen) {
+//       video.msRequestFullscreen()
+//     }
+//   }
+// }
 const validUrl = () => {
   const re = /https:\/\/www\.bilibili\.com\/video\/.*/
   const re2 = /https:\/\/www\.bilibili\.com\/list\/.*/
@@ -99,20 +152,39 @@ export const closeDm = () => {
   }, 1000)
 }
 
+
+//  监听暂停播放事件 如果是由space键暂停的  或者click 事件触发的 则跳过
+const onListenPause = (isPauseByUser: boolean) => {
+
+  
+}
 export const checkStop = () => {
-  let playTime = 0
-  const timer4Play = setInterval(() => {
-    if (playTime > 6) {
-      clearInterval(timer4Play)
-      return
+  const video = document.querySelector("video")
+  if(!video) return
+  window.addEventListener('keydown', (event)=> {
+    const video = document.querySelector("video")
+  if(!video) return
+  const isPauseByUser = !video.paused
+    // 先确定播放状态
+    if(event.code === 'Space') {
+      chromeStorage.set({isPauseByUser })
     }
-    // const playBtn = document.querySelector(".bpx-player-ctrl-btn.bpx-player-ctrl-play")
-    const playBtn = $(".bpx-player-ctrl-btn.bpx-player-ctrl-play")
-    if (playBtn.length > 0) {
-      playBtn.trigger("click")
-      playTime++
-    }
-  }, 1000)
+  })
+  video.addEventListener('click', (event)=> {
+    const video = document.querySelector("video")
+    if(!video) return
+    const isPauseByUser = !video.paused
+    chromeStorage.set({isPauseByUser })
+  })
+  // setInterval(async () => {
+  //   const isPauseByUser = await chromeStorage.get("isPauseByUser") as boolean
+  //   if(isPauseByUser) return
+  //   const video = document.querySelector("video")
+  //   if(!video) return
+  //   if(video.paused) {
+  //     video.play()
+  //   }
+  // }, 1000)
 }
 
 // export const checkPlay = () => {
--- src/utils/injectFn.ts
+++ src/utils/injectFn.ts
@@ -11,25 +11,52 @@ export const injectFile = () => {
 
 //  b站注入  覆写 原生 定期函数
 export const injectForBilibili = async () => {
+  window.console.error = null
+//   setTimeout(()=> {
+//       window.UserStatus.userInfo = {
+//     "isLogin": true
+// }
+//   }, 10*1000)
   //  b站覆写setTimeout
   const originSetTimeout = window.setTimeout
   window.setTimeout = function (func: (args: void) => void, delay: number | undefined) {
     let newdelay = delay
-    // if (delay === 4e3) newdelay = 4e8
-    if (delay === 3e4) newdelay = 10e10
+    if (delay === 4e3) newdelay = 4e8
+    if (delay === 3e4 ) newdelay = 10e10
     return originSetTimeout.call(this, func, newdelay) as NodeJS.Timeout
   }
 
+  const originSetInterval = window.setInterval
+  // let ttt = 0
+
+  // setInterval(()=> {
+  //   ttt+=1
+  // },1000)
+  // setTimeout(()=>{
+    window.setInterval = function (func: (args: void) => void, delay: number | undefined){
+      let newdelay = delay || 11111
+      // if(ttt > 60 && ttt < 75){
+        console.log("🚀 ~ injectForBilibili ~ttt===== newdelay:", newdelay)
+      // }
+      if (newdelay === 1500) newdelay = 10e10
+      // if ( newdelay <= 1000 ) newdelay = 10e10/
+      // if ( newdelay <= 1000 && newdelay != 250) newdelay = 10e10
+      // if ( newdelay == 250) newdelay = 10e10
+      console.log("🚀 ~ injectForBilibili ~ttt=====222222222 newdelay:", newdelay)
+
+      return originSetInterval.call(this, func, newdelay) as NodeJS.Timeout
+    }
+  // }, 6000)
   const originSetItem = window.localStorage.setItem
   window.localStorage.setItem = function (key, value) {
     if (key === "bpx_player_profile") {
       const profile = JSON.parse(value)
       // 默认关闭弹幕
-      profile.dmSetting.dmSwitch = false
+      // profile.dmSetting.dmSwitch = false
       profile.lastView = Date.now() + 864e10
       // 新增属性变更  解除b站试看一次限制
       profile.lastUnlogintrialView = Date.now() + 864e10 - 11
-      profile.media.quality = "80"
+      profile.media.quality = 80
       value = JSON.stringify(profile)
     }
     originSetItem.call(this, key, value)
